4ada2e4c1674537f21680af4f615c4e40fe6e9cf
Joel Kuzmarski <leoj3n@gmail.com>
2022-03-05 14:38:03 -0600

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# example-container-lib-app
+
+```
+yarn install && yarn start
+```
---
+++ example-app/src/index.ts
@@ -0,0 +1,11 @@
+// @ts-ignore
+/* eslint-disable node/no-missing-import */
+import MyLib from 'example-lib'
+//import 'example-lib/style.css'
+//import { createApp } from 'vue'
+
+console.log('MyLib is', MyLib);
+MyLib('#app');
+
+//const app = createApp(CompA)
+//app.mount('#app')
